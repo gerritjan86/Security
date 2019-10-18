8e5dfc8f2c53728a61932c7e530d105b284b0e95
ODIN\gerritjans <gerrit.jan.stam@gmail.com>
2019-10-18 09:25:21 +0200

    Clicking captcha generates new captcha

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,7 +37,7 @@ namespace WebAppSecurity.Controllers
 		[Route("get-captcha-image")]
 		public IActionResult GetCaptchaImage()
 		{
-			int width = 100;
+			int width = 140;
 			int height = 36;
 			var captchaCode = Captcha.GenerateCaptchaCode();
 			var result = Captcha.GenerateCaptchaImage(width, height, captchaCode);
